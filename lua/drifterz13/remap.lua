adbba7e784f5cff214af5a8ce42352e30343d4d0
drifterz13 <mixmyxxe@gmail.com>
2024-08-13 20:23:16 +0700

    remap jump up/dow via ctrl+d/u

--- lua/drifterz13/remap.lua
+++ lua/drifterz13/remap.lua
@@ -1,2 +1,5 @@
 vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { noremap = true })
+vim.keymap.set('n', '<C-n>', '<C-n>zz', { noremap = true })
