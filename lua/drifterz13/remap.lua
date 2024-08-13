d2a389bac7b4d2ca4c37bb1de07f44f4198efefb
drifterz13 <mixmyxxe@gmail.com>
2024-08-13 20:27:03 +0700

    remap jump paragraph

--- lua/drifterz13/remap.lua
+++ lua/drifterz13/remap.lua
@@ -3,3 +3,5 @@ vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 
 vim.keymap.set('n', '<C-d>', '<C-d>zz', { noremap = true })
 vim.keymap.set('n', '<C-n>', '<C-n>zz', { noremap = true })
+vim.keymap.set('n', '{', '{zz', { noremap = true })
+vim.keymap.set('n', '}', '}zz', { noremap = true })
