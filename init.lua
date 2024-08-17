98ee8b29165d45911db1fa5fd0d2dc74c17aaf45
drifterz13 <mixmyxxe@gmail.com>
2024-08-17 23:38:16 +0700

    fix: remap <leader>pv does not work

--- init.lua
+++ init.lua
@@ -2,8 +2,12 @@ require("drifterz13.lazy")
 require("drifterz13.colors")
 
 vim.api.nvim_set_option("clipboard", "unnamed")
-local colors = require('drifterz13.colors')
 
+-- Register a command to switch to terminal background.
+local colors = require('drifterz13.colors')
 vim.api.nvim_create_user_command('ColorMyPencils', function(opts)
   colors.ColorMyPencils(opts.args ~= "")
-end, {}) 
+end, {})
+
+-- Custom keymaps
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
