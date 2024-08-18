41c70d64ad639ba441f1f238a77993aca963558b
drifterz13 <mixmyxxe@gmail.com>
2024-08-18 16:37:03 +0700

    chore: update configure

--- lua/plugins/chatgpt.lua
+++ lua/plugins/chatgpt.lua
@@ -4,8 +4,9 @@ return {
     event = "VeryLazy",
     config = function()
       require("chatgpt").setup({
+        api_key_cmd = "gpg --decrypt " .. vim.fn.stdpath("config") .. "/chatgpt-api-key.gpg",
         openai_params = {
-          model = "gpt-3.5-turbo",
+          model = "gpt-4o-mini",
           frequency_penalty = 0,
           presence_penalty = 0,
           max_tokens = 4095,
@@ -19,6 +20,21 @@ return {
       "MunifTanjim/nui.nvim",
       "nvim-lua/plenary.nvim",
       "nvim-telescope/telescope.nvim"
+    },
+    keys = {
+      { "<leader>c",  "<cmd>ChatGPT<CR>",                              { desc = "ChatGPT" } },
+      { "<leader>ce", "<cmd>ChatGPTEditWithInstruction<CR>",           { desc = "Edit with instruction", mode = { "n", "v" } } },
+      { "<leader>cg", "<cmd>ChatGPTRun grammar_correction<CR>",        { desc = "Grammar Correction", mode = { "n", "v" } } },
+      { "<leader>ct", "<cmd>ChatGPTRun translate<CR>",                 { desc = "Translate", mode = { "n", "v" } } },
+      { "<leader>ck", "<cmd>ChatGPTRun keywords<CR>",                  { desc = "Keywords", mode = { "n", "v" } } },
+      { "<leader>cd", "<cmd>ChatGPTRun docstring<CR>",                 { desc = "Docstring", mode = { "n", "v" } } },
+      { "<leader>ca", "<cmd>ChatGPTRun add_tests<CR>",                 { desc = "Add Tests", mode = { "n", "v" } } },
+      { "<leader>co", "<cmd>ChatGPTRun optimize_code<CR>",             { desc = "Optimize Code", mode = { "n", "v" } } },
+      { "<leader>cs", "<cmd>ChatGPTRun summarize<CR>",                 { desc = "Summarize", mode = { "n", "v" } } },
+      { "<leader>cf", "<cmd>ChatGPTRun fix_bugs<CR>",                  { desc = "Fix Bugs", mode = { "n", "v" } } },
+      { "<leader>cx", "<cmd>ChatGPTRun explain_code<CR>",              { desc = "Explain Code", mode = { "n", "v" } } },
+      { "<leader>cr", "<cmd>ChatGPTRun roxygen_edit<CR>",              { desc = "Roxygen Edit", mode = { "n", "v" } } },
+      { "<leader>cl", "<cmd>ChatGPTRun code_readability_analysis<CR>", { desc = "Code Readability Analysis", mode = { "n", "v" } } },
     }
   }
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -14,11 +14,11 @@ return {
         },
       })
 
-      vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-      vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+      vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Project files' })
+      vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Git files' })
       vim.keymap.set('n', '<leader>ps', function()
         builtin.grep_string({ search = vim.fn.input('Grep > ') });
-      end)
+      end, { desc = 'Grep string' })
     end
   },
 }
