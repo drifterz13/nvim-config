283d561a6510f6741bab25e7c8df31734fa2d065
totsawat <totsawat@SklillLanes-Laptop-2.local>
2024-08-20 21:47:57 +0700

    replace chatgpt plugin with copilotchat

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -60,6 +60,16 @@ return {
         -- Prefix image names with timestamp.
         return string.format("%s-", os.time())
       end,
-    }
+    },
+
+    -- URL it will be ignored but you can customize this behavior here.
+    ---@param url string
+    follow_url_func = function(url)
+      -- Open the URL in the default web browser.
+      vim.fn.jobstart({ "open", url }) -- Mac OS
+      -- vim.fn.jobstart({"xdg-open", url})  -- linux
+      -- vim.cmd(':silent exec "!start ' .. url .. '"') -- Windows
+      -- vim.ui.open(url) -- need Neovim 0.10.0+
+    end,
   },
 }
