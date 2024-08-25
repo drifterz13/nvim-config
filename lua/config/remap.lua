495549fc1dad22818c582bdc8903c70af7fe8df3
drifterz13 <mixmyxxe@gmail.com>
2024-08-25 09:45:30 +0700

    update remap

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -3,3 +3,4 @@ vim.g.maplocalleader = "\\"
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true })
 vim.keymap.set("n", "{", "{zz", { noremap = true })
 vim.keymap.set("n", "}", "}zz", { noremap = true })
+vim.keymap.set("n", "<C-x>" ,":bd<CR>", { noremap = true })
