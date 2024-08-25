0d71c796e8195f57300a5f86d638a166604af576
drifterz13 <mixmyxxe@gmail.com>
2024-08-25 23:17:47 +0700

    add cluade snippet

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -4,10 +4,19 @@ return {
     event = "VeryLazy",
     build = "make",
     opts = {
-      provider = "openai",
-      openai = {
-        endpoint = "https://api.openai.com/v1",
-        model = "gpt-4o-2024-08-06",
+      -- provider = "openai",
+      -- openai = {
+      --   endpoint = "https://api.openai.com/v1",
+      --   model = "gpt-4o-2024-08-06",
+      --   timeout = 30000, -- Timeout in milliseconds
+      --   temperature = 0,
+      --   max_tokens = 4096,
+      --   ["local"] = false,
+      -- },
+      provider = "claude",
+      claude = {
+        endpoint = "https://api.anthropic.com",
+        model = "claude-3-5-sonnet-20240620",
         timeout = 30000, -- Timeout in milliseconds
         temperature = 0,
         max_tokens = 4096,
